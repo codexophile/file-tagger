8b833352cd723d450af5459eaff48ca30472ec67
Codexophile <jaliya-github@incogneato.us>
2024-12-28 08:54:49 +0530

    fix: correct path to tags.ini in configuration

--- src/config/paths.js
+++ src/config/paths.js
@@ -1,6 +1,6 @@
 const path = require( 'path' );
 
 module.exports = {
-  tagsIniPath: path.join( __dirname, '..', 'tags.ini' ),
+  tagsIniPath: path.join( __dirname, '../..', 'tags.ini' ),
   defaultIniPath: './tags.ini'
 };
